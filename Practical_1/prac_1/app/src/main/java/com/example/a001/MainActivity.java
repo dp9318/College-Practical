2e4320bce3b8c079450efe36221806072dad72b8
Dipankar Kumar <128923498+dp9318@users.noreply.github.com>
2024-02-13 17:33:11 +0530

    Add files via upload

---
+++ Practical_1/prac_1/app/src/main/java/com/example/a001/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.a001;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.my_text_view);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        textView.setText(item.getTitle().toString());
+        return true;
+
+    }
+}
